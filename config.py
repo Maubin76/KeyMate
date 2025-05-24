6aa0182a343f3b3047bec3cf91b3638330b6157a
Mathieu Aubin <maubin@enssat.fr>
2025-05-24 12:26:44 +0200

    feat(config) : configuration variables

---
+++ config.py
@@ -0,0 +1,5 @@
+# global configuration for the password manager
+# -*- coding: utf-8 -*-
+MASTER_PASSWORD = "motdepassefort"
+ENCRYPTION_KEY = b"0123456789abcdef0123456789abcdef"  # 32 bytes for AES-256
+ENC_FILE = "passwords.enc"
