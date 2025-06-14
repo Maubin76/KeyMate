97ce15980f42c81e2f86f6ca942c569018787d70
Mathieu Aubin <maubin@enssat.fr>
2025-06-14 22:03:20 +0200

    feat(config): move master password and encryption key to usb key and passwords to OneDrive

--- config.py
+++ config.py
@@ -1,5 +1,15 @@
+from pathlib import Path
 # global configuration for the password manager
 # -*- coding: utf-8 -*-
-MASTER_PASSWORD = "b4422a9eb0bce1b0582afba7664af01b999bbdc4b977aa85dbd6638426f85f15" # SHA-256 hash of the master password
-ENCRYPTION_KEY = b"K8v4j6PEyO7vqV5nGzPy3U0dD1m8KxXFSUKrxtXzC4I="  # 32 bytes for AES-256
-ENC_FILE = "passwords.enc" # File to store encrypted passwords
+USB_KEY_PATH = Path("E:") / "KeyMate"
+
+def read_file_on_usb_key(filename: str):
+    if not USB_KEY_PATH.exists():
+        raise FileNotFoundError(f"Clé USB non détectée")
+    
+    with open(USB_KEY_PATH / filename, "rb") as file:
+        return file.read()
+
+MASTER_PASSWORD =  read_file_on_usb_key("master_password.hash").decode() # SHA-256 hash of the master password
+ENCRYPTION_KEY = read_file_on_usb_key("encryption_key.enc") # 32 bytes for AES-256
+ENC_FILE = Path.home() / "OneDrive" / "KeyMate" / "passwords.enc" # File to store encrypted passwords
